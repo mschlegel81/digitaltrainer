8237e224925c7d0bc0bf0cdc2e6c2765ca4e0d61
Martin Schlegel <m.schlegel81@googlemail.com>
2023-03-15 21:22:17 +0100

    Added ROM/RAM import/export functionality

--- romeditorunit.pas
+++ romeditorunit.pas
@@ -6,26 +6,30 @@ INTERFACE
 
 USES
   Classes, sysutils, Forms, Controls, Graphics, Dialogs, Grids, StdCtrls,
-  ExtCtrls,logicalGates;
+  ExtCtrls, Menus,logicalGates;
 
 TYPE
 
   { TRomEditorForm }
-  //TODO: Add import/export functionality (csv)
   TRomEditorForm = class(TForm)
+    MainMenu1: TMainMenu;
+    miExport: TMenuItem;
+    miImport: TMenuItem;
+    OpenDialog1: TOpenDialog;
     propRemDataLabel: TLabel;
     propRemDataShape: TShape;
     propOkLabel: TLabel;
     propAddDataLabel: TLabel;
     propOkShape: TShape;
     propAddDataShape: TShape;
+    SaveDialog1: TSaveDialog;
     StringGrid1: TStringGrid;
     PROCEDURE FormCreate(Sender: TObject);
-    PROCEDURE propAddDataShapeMouseDown(Sender: TObject; button: TMouseButton;
-      Shift: TShiftState; X, Y: integer);
+    PROCEDURE miExportClick(Sender: TObject);
+    PROCEDURE miImportClick(Sender: TObject);
+    PROCEDURE propAddDataShapeMouseDown(Sender: TObject; button: TMouseButton;  Shift: TShiftState; X, Y: integer);
     PROCEDURE propOkShapeMouseDown(Sender: TObject; button: TMouseButton; Shift: TShiftState; X, Y: integer);
-    PROCEDURE propRemDataShapeMouseDown(Sender: TObject; button: TMouseButton;
-      Shift: TShiftState; X, Y: integer);
+    PROCEDURE propRemDataShapeMouseDown(Sender: TObject; button: TMouseButton; Shift: TShiftState; X, Y: integer);
     PROCEDURE StringGrid1ValidateEntry(Sender: TObject; aCol, aRow: integer; CONST oldValue: string; VAR newValue: string);
   private
     data:T_wireValueArray;
@@ -70,6 +74,44 @@ PROCEDURE TRomEditorForm.FormCreate(Sender: TObject);
     setLength(data,0);
   end;
 
+PROCEDURE TRomEditorForm.miExportClick(Sender: TObject);
+  begin
+    if SaveDialog1.execute then begin
+      StringGrid1.SaveToCSVFile(SaveDialog1.fileName,';');
+    end;
+  end;
+
+PROCEDURE TRomEditorForm.miImportClick(Sender: TObject);
+  VAR col,row:longint;
+      restored: TGridColumn;
+      rowOk:boolean=false;
+      fixedValue: string;
+  begin
+    if OpenDialog1.execute then begin
+      StringGrid1.LoadFromCSVFile(OpenDialog1.fileName,';',false,1);
+      while StringGrid1.Columns.count>3 do StringGrid1.Columns.delete(3);
+      while StringGrid1.Columns.count<3 do begin
+        restored:=StringGrid1.Columns.add;
+        restored.Alignment:=taRightJustify;
+        if StringGrid1.Columns.count=3 then restored.title.caption:='2cmp' else
+        if StringGrid1.Columns.count=2 then restored.title.caption:='dec' else
+                                            restored.title.caption:='bin';
+      end;
+      setLength(data,StringGrid1.rowCount-1);
+      for row:=1 to StringGrid1.rowCount-1 do begin
+        rowOk:=false;
+        for col:=1 to 3 do if not (rowOk) then begin
+          fixedValue:=StringGrid1.Cells[col,row];
+          StringGrid1ValidateEntry(Sender,col,row,StringGrid1.Cells[col,row],fixedValue);
+          if StringGrid1.Cells[col,row]=fixedValue
+          then rowOk:=true
+          else StringGrid1.Cells[col,row]:=fixedValue;
+        end;
+      end;
+      fillTable;
+    end;
+  end;
+
 PROCEDURE TRomEditorForm.propAddDataShapeMouseDown(Sender: TObject; button: TMouseButton; Shift: TShiftState; X, Y: integer);
   begin
     if length(data)<65536 then begin
@@ -89,9 +131,8 @@ PROCEDURE TRomEditorForm.StringGrid1ValidateEntry(Sender: TObject; aCol, aRow: i
       3: wire:=parseWire2Complement(newValue,16);
     else wire:=data[aRow];
     end;
-
     data[aRow]:=wire;
-    updateDataRow(aRow,wire);
+    if Sender is TStringGrid then updateDataRow(aRow,wire);
   end;
 
 PROCEDURE TRomEditorForm.updateDataRow(CONST adress: longint; CONST wireValue: T_wireValue);
